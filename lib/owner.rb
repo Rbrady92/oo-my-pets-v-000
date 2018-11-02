d6d515ecd722a966123b36968b6a539b2624fefe
Ryan Brady <ryanbrady369@gmail.com>
2018-11-02 21:29:11 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -9,31 +9,34 @@ class Owner
     @@all << self
     @pets = {fishes: [], cats: [], dogs: []}
     #@name = name
-  end 
+  end
 
   def say_species
     @species
-  end 
+  end
 
   def reset_all
     @@all = []
-  end 
+  end
 
   def buy_cat(name)
     cat = Cat.new(name)
     @pets[:cats] << cat
-  end 
+  end
 
   def buy_dog(name)
     dog = Dog.new(name)
-    @pets[:dogs] << dog 
-  end 
+    @pets[:dogs] << dog
+  end
 
   def buy_fish(name)
     fish = Fish.new(name)
-    @pets[:dogs] << dog 
-  end 
+    @pets[:dogs] << dog
+  end
 
+  def list_pets
+    puts "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
+  end 
 
   private
 
