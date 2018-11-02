5e54496e209879ba14d8185b41d11435d1ea21a8
Ryan Brady <ryanbrady369@gmail.com>
2018-11-02 21:47:32 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -59,8 +59,8 @@ class Owner
     "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
   end
 
-  def self.count
-    Owner.all.length
+  def count
+    @@all.length
   end
 
   private
