445a366bb82d2cd5d48cc287627be199b46c8e65
Ryan Brady <ryanbrady369@gmail.com>
2018-11-02 21:25:20 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,42 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  @@all = []
+
+  def initialize(species)
+    @species = species
+    @@all << self
+    @pets = {fishes: [], cats: [], dogs: []}
+    #@name = name
+  end 
+
+  def say_species
+    @species
+  end 
+
+  def reset_all
+    @@all = []
+  end 
+
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
+  end 
+
+  def buy_dog(name)
+    dog = Dog.new(name)
+    @pets[:dogs] << dog 
+  end 
+
+  def buy_fish(name)
+    fish = Fish.new(name)
+    @pets[:dogs] << dog 
+  end 
+
+
+  private
+
+  attr_writer :species
+
 end
